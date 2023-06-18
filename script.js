88a4b69b2129b261f483ebab31bd8357adf77ec3
BatuMiras <batu.mirascioglu@gmail.com>
2023-06-18 18:08:38 +0300

    user login
    user login

---
+++ script.js
@@ -0,0 +1,15 @@
+var x = document.getElementById("Login");
+var z = document.getElementById("Register");
+var y = document.getElementById("btn");
+
+function register() {
+    x.style.left = "-400px";
+    z.style.left = "50px";
+    y.style.left = "110px";
+}
+
+function login() {
+    x.style.left = "50px";
+    z.style.left = "450px";
+    y.style.left = "0px";
+}
